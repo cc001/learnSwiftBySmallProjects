827ab15b865509db010bfaf173a0053e2ff38508
CC <iechenchuang@163.com>
2016-12-14 18:05:31 +0800

    定制字体bug暂时搁置提交

---
+++ CustomFont/CustomFont/TableViewController.swift
@@ -0,0 +1,154 @@
+//
+//  TableViewController.swift
+//  CustomFont
+//
+//  Created by 陈闯 on 2016/12/14.
+//  Copyright © 2016年 CC. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    //声明各种变量
+    var stringArray = ["30 Days Swift", "这些字体特别适合打「奋斗」和「理想」", "谢谢「造字工房」，本案例不涉及商业使用", "使用到造字工房劲黑体，致黑体，童心体", "呵呵，再见🤗 See you next Project", "微博 @Allen朝辉",
+                       "测试测试测试测试测试测试",
+                       "123",
+                       "Alex",
+                       "@@@@@@"]
+    
+    var fontNames =  ["MFTongXin_Noncommercial-Regular", "MFJinHei_Noncommercial-Regular", "MFZhiHei_Noncommercial-Regular", "edundot", "Gaspar Regular"]
+
+    var fontRowIndex = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.title = "各种各样的字体"
+        
+        for family in UIFont.familyNames {
+            for font in UIFont.fontNames(forFamilyName: family){
+                print(font)
+            }
+        }
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "切换", style: UIBarButtonItemStyle.plain, target: self, action: #selector(changeFontEvent))
+     
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "tableViewCell")
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+        tableView.backgroundColor = UIColor.black
+        
+        
+    }
+
+    
+    func changeFontEvent() {
+        fontRowIndex = (fontRowIndex + 1) % 5
+        print(fontNames[fontRowIndex])
+        tableView.reloadData()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return stringArray.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+        cell.backgroundColor = UIColor.black
+
+//        // Configure the cell...
+//        cell.textLabel?.text = stringArray[indexPath.row]
+//        cell.textLabel?.textColor = UIColor.white
+//        //cell.textLabel?.font = UIFont(name:self.fontNames[fontRowIndex], size:16)
+        
+        let label = UILabel.init(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+        label.text = "这里给出测试字体为的是测试出更好的样子来"
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "KohinoorTelugu-Regular", size: 20)
+        cell.addSubview(label)
+        
+        
+//        if indexPath.row == 0 {
+//            cell.textLabel?.font = UIFont(name: "KohinoorTelugu-Regular", size: 20)
+//        }
+//        else if indexPath.row == 1 {
+//            cell.textLabel?.font = UIFont(name: "CourierNewPS-ItalicMT", size: 20)
+//        }else if indexPath.row == 2 {
+//            cell.textLabel?.font = UIFont(name: "HelveticaNeue-Italic", size: 20)
+//        }else if indexPath.row == 3 {
+//            cell.textLabel?.font = UIFont(name: "BodoniSvtyTwoOSITCTT-BookIt", size: 20)
+//        }else if indexPath.row == 4 {
+//            cell.textLabel?.font = UIFont(name: "HiraginoSans-W3", size: 20)
+//        }
+
+        
+        
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
