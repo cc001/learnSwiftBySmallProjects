f9108d9af1383fa11224dc362480e8652ce32fe2
CC <iechenchuang@163.com>
2016-12-14 15:50:53 +0800

    CustomFont项目初始化

---
+++ CustomFont/CustomFont/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomFont
+//
+//  Created by 陈闯 on 2016/12/14.
+//  Copyright © 2016年 CC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
