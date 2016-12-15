d558c7b96b1a4c20a740424a2f59a5aabd0329b2
CC <iechenchuang@163.com>
2016-12-15 18:07:32 +0800

    下班回家继续写

---
+++ CarouselEffect/CarouselEffect/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CarouselEffect
+//
+//  Created by 陈闯 on 2016/12/15.
+//  Copyright © 2016年 CC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
