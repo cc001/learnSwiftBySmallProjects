fce29ebc9ac08bd4d5929eb1e1b019274b56b0c3
CC <iechenchuang@163.com>
2016-12-28 15:04:46 +0800

    侧滑菜单提交

---
+++ VenderSlideMenu/Pods/Target Support Files/Pods-VenderSlideMenu/Pods-VenderSlideMenu-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VenderSlideMenu : NSObject
+@end
+@implementation PodsDummy_Pods_VenderSlideMenu
+@end
---
+++ VenderSlideMenu/VenderSlideMenu/RightViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RightViewController.swift
+//  VenderSlideMenu
+//
+//  Created by 陈闯 on 2016/12/28.
+//  Copyright © 2016年 CC. All rights reserved.
+//
+
+import UIKit
+
+class RightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
